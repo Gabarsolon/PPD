99f1c68f192c055cefd3fa283fda86954dd11102
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-10-19 22:44:15 +0300

    LAB-1: Added locks

--- Lab1-Non-cooperative-multithreading/src/Main.java
+++ Lab1-Non-cooperative-multithreading/src/Main.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /*
 3. Summation with fixed structure of inputs
@@ -20,17 +23,68 @@ From time to time, as well as at the end, a consistency check shall be performed
 
 Two updates involving distinct variables must be able to proceed independently (without having to wait for the same mutex).
 */
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Random rand = new Random();
+    static ObservableVariable A;
+    static ObservableVariable B;
+    static ObservableVariable C;
+    static ObservableVariable D;
+    static ObservableVariable E;
+    static ObservableVariable F;
+    static Random rand = new Random();
 
-        ObservableVariable A = new ObservableVariable(10);
-        ObservableVariable B = new ObservableVariable(15);
-        ObservableVariable C = new ObservableVariable(20);
+    static Lock lockForA = new ReentrantLock();
+    static Lock lockForB = new ReentrantLock();
+    static Lock lockForC = new ReentrantLock();
 
-        ObservableVariable D = new ObservableVariable(Arrays.asList(A,B));
-        ObservableVariable E = new ObservableVariable(Arrays.asList(B,C));
-        ObservableVariable F = new ObservableVariable(Arrays.asList(D,A));
+    private static void threadFunction(int threadIndex){
+        while(true){
+//            System.out.println("Thread " + threadIndex + " is doing stuff");
+            Integer primaryVariableIndex = rand.nextInt(3);
+            Integer newValue = rand.nextInt(100);
+            String variableName = "";
+            switch(primaryVariableIndex){
+                case 0:
+                    lockForA.lock();
+                    A.setVariable(newValue);
+                    lockForA.unlock();
+                    variableName = "A";
+                    break;
+                case 1:
+                    lockForB.lock();
+                    B.setVariable(newValue);
+                    lockForB.unlock();
+                    variableName = "B";
+                    break;
+                case 2:
+                    lockForC.lock();
+                    C.setVariable(newValue);
+                    lockForC.unlock();
+                    variableName = "C";
+                    break;
+            }
+//            System.out.printf("Thread %d modified variable %s\n", threadIndex, variableName);
+//            System.out.println("A=" + A.value);
+//            System.out.println("B=" + B.value);
+//            System.out.println("C=" + C.value);
+//            System.out.println("D=" + D.value);
+//            System.out.println("E=" + E.value);
+//            System.out.println("F=" + F.value);
+//            System.out.println(D.consistencyCheck());
+//            System.out.println(E.consistencyCheck());
+//            System.out.println(F.consistencyCheck());
+//            System.out.println();
+        }
+    }
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        A = new ObservableVariable(10);
+        B = new ObservableVariable(15);
+        C = new ObservableVariable(20);
+
+        D = new ObservableVariable(Arrays.asList(A,B));
+        E = new ObservableVariable(Arrays.asList(B,C));
+        F = new ObservableVariable(Arrays.asList(D,A));
 
         A.variablesToNotify.add(D);
         B.variablesToNotify.add(D);
@@ -41,31 +95,26 @@ public class Main {
         D.variablesToNotify.add(F);
         A.variablesToNotify.add(F);
 
+        int nThreads = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+
+        for(int threadIndex = 0; threadIndex < nThreads; threadIndex++) {
+            int finalThreadIndex = threadIndex;
+            executorService.execute(() -> threadFunction(finalThreadIndex));
+        }
+
         while(true){
-            Integer primaryVariableIndex = rand.nextInt(3);
-            Integer newValue = rand.nextInt(100);
-            switch(primaryVariableIndex){
-                case 0:
-                    A.setVariable(newValue);
-                    break;
-                case 1:
-                    B.setVariable(newValue);
-                    break;
-                case 2:
-                    C.setVariable(newValue);
-                    break;
-            }
-            System.out.println("A=" + A.value);
-            System.out.println("B=" + B.value);
-            System.out.println("C=" + C.value);
-            System.out.println("D=" + D.value);
-            System.out.println("E=" + E.value);
-            System.out.println("F=" + F.value);
+            Thread.sleep(500);
+            lockForA.lock();
+            lockForB.lock();
+            lockForC.lock();
             System.out.println(D.consistencyCheck());
             System.out.println(E.consistencyCheck());
             System.out.println(F.consistencyCheck());
             System.out.println();
-            Thread.sleep(100);
+            lockForA.unlock();
+            lockForB.unlock();
+            lockForC.unlock();
         }
     }
 }
--- Lab1-Non-cooperative-multithreading/src/ObservableVariable.java
+++ Lab1-Non-cooperative-multithreading/src/ObservableVariable.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ObservableVariable {
-    public Integer value;
+    public static Lock lock = new ReentrantLock();
+    public int value;
     public List<ObservableVariable> variablesToNotify;
     public List<ObservableVariable> variablesToWatch;
     public ObservableVariable(Integer value){
@@ -20,19 +23,31 @@ public class ObservableVariable {
         );
     }
     public void setVariable(Integer newValue){
+        lock.lock();
         Integer differenceBetweenOldValueAndNewValue = newValue - value;
         value = newValue;
         notifyVariables(differenceBetweenOldValueAndNewValue);
-    }
-    public void updateVariable(Integer newValue){
-        this.value += newValue;
-        notifyVariables(newValue);
+        lock.unlock();
     }
 
-    public Integer computeSum(){
+    public void updateVariable(Integer newValue){
+        lock.lock();
+        this.value += newValue;
+        notifyVariables(newValue);
+        lock.unlock();
+
+    }
+
+    public int computeSum(){
+
        return variablesToWatch.stream().map((variable) -> variable.value).reduce(0, Integer::sum);
     }
+
     public boolean consistencyCheck(){
-        return this.value == computeSum();
+        lock.lock();
+        int sum = computeSum();
+        System.out.printf("Current value %s\n Real value %s\n", this.value, sum);
+        lock.unlock();
+        return this.value == sum;
     }
 }
