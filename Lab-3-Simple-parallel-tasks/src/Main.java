cba7327eb632b27e1ebb4943b10eb10b00c73f46
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-11-07 19:46:14 +0200

    LAB-3: Added the task function and the one for computing the product

--- Lab-3-Simple-parallel-tasks/src/Main.java
+++ Lab-3-Simple-parallel-tasks/src/Main.java
@@ -7,23 +7,42 @@ public class Main {
     static int noOfColumnsFirstMatrix = 9;
     static int noOfRowsSecondMatrix = 9;
     static int noOfColumnsSecondMatrix = 9;
-    static int[][] firstMatrix = new int[noOfRowsFirstMatrix][noOfColumnsFirstMatrix];
-    static int[][] secondMatrix = new int[noOfRowsSecondMatrix][noOfColumnsSecondMatrix];
-    static int[][] resultMatrix = new int[noOfRowsFirstMatrix][noOfColumnsSecondMatrix];
+    static int[][] firstMatrix;
+    static int[][] secondMatrix;
+    static int[][] resultMatrix;
     static Random rand = new Random();
+
     static void addRandomNumbersToMatrix(int[][] matrix) {
         int noOfRows = matrix.length;
         int noOfColumns = matrix[0].length;
 
         for (int rowIndex = 0; rowIndex < noOfRows; rowIndex++)
-            for(int columnIndex = 0; columnIndex < noOfColumns; columnIndex++)
+            for (int columnIndex = 0; columnIndex < noOfColumns; columnIndex++)
                 matrix[rowIndex][columnIndex] = rand.nextInt(10);
     }
-    static void printMatrix(int[][] matrix){
-        for(int[] row : matrix)
+
+    static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix)
             System.out.println(Arrays.toString(row));
         System.out.println();
     }
+
+    static int computeElementOfResultingMatrix(int rowFromFirstMatrix, int columnFromSecondMatrix) {
+        int sumOfProductsBetweenRowFromFirstMatrixAndColumnFromSecondMatrix = 0;
+
+        for (int index = 0; index < noOfRowsSecondMatrix; index++)
+            sumOfProductsBetweenRowFromFirstMatrixAndColumnFromSecondMatrix
+                    += firstMatrix[rowFromFirstMatrix][index] * secondMatrix[index][columnFromSecondMatrix];
+
+        return sumOfProductsBetweenRowFromFirstMatrixAndColumnFromSecondMatrix;
+    }
+
+    static void matrixProduct() {
+        for (int i = 0; i < noOfRowsFirstMatrix; i++)
+            for(int j=0; j<noOfColumnsSecondMatrix; j++)
+                resultMatrix[i][j] = computeElementOfResultingMatrix(i,j);
+    }
+
     public static void main(String[] args) {
         var scanner = new Scanner(System.in);
 
@@ -39,10 +58,22 @@ public class Main {
         System.out.print("Input the number of columns for the second matrix: ");
         noOfColumnsSecondMatrix = scanner.nextInt();
 
+        firstMatrix = new int[noOfRowsFirstMatrix][noOfColumnsFirstMatrix];
+        secondMatrix = new int[noOfRowsSecondMatrix][noOfColumnsSecondMatrix];
+        resultMatrix = new int[noOfRowsFirstMatrix][noOfColumnsSecondMatrix];
+
         addRandomNumbersToMatrix(firstMatrix);
         addRandomNumbersToMatrix(secondMatrix);
 
+        matrixProduct();
+
+        System.out.println("First matrix: ");
         printMatrix(firstMatrix);
+
+        System.out.println("Second matrix: ");
         printMatrix(secondMatrix);
+
+        System.out.println("Result matrix: ");
+        printMatrix(resultMatrix);
     }
 }
