a582325c56a690db4213c078116a5188e9a0d876
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-12-06 22:57:57 +0200

    LAB-5: Added parralell n^2, random poly generator

--- Lab-5-Parallelizing-techniques/src/Main.java
+++ Lab-5-Parallelizing-techniques/src/Main.java
@@ -1,13 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicIntegerArray;
 import java.util.stream.Collectors;
 
 public class Main {
+    static int MAX_COEFFICIENT = 1000;
+    static int MAX_DEGREE = 100;
     static void printPolynomial(int[] polynomial) {
         var stringBuilder = new StringBuilder("%d".formatted(polynomial[0]));
 
         for (int i = 1; i < polynomial.length; i++) {
             stringBuilder.append(" + %d%s".formatted(polynomial[i], "x^%d".formatted(i)));
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    static int[] generateRandomPolynomial(){
+        var random = new Random();
+        int degree = random.nextInt(MAX_DEGREE);
+        int[] polynomial = new int[degree];
+        for(int i = 0;i<degree;i++){
+            polynomial[i] = random.nextInt(MAX_COEFFICIENT);
+        }
+        return polynomial;
+    }
+
+    static void printPolynomial(AtomicIntegerArray polynomial) {
+        var stringBuilder = new StringBuilder("%d".formatted(polynomial.get(0)));
+
+        for (int i = 1; i < polynomial.length(); i++) {
+            stringBuilder.append(" + %d%s".formatted(polynomial.get(i), "x^%d".formatted(i)));
         }
 
         System.out.println(stringBuilder);
@@ -28,9 +54,33 @@ public class Main {
         printPolynomial(product);
     }
 
+    static void regularMultiplicationParallel(int[] polynomial1, int[] polynomial2) {
+        int polynomial1Degree = polynomial1.length;
+        int polynomial2Degree = polynomial2.length;
+
+        var product = new AtomicIntegerArray(polynomial1Degree + polynomial2Degree - 1);
+
+        List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
+
+        for (int i = 0; i < polynomial1Degree; i++)
+            for (int j = 0; j < polynomial2Degree; j++) {
+                int finalI = i;
+                int finalJ = j;
+                completableFutures.add(CompletableFuture.supplyAsync(() -> {
+                    product.addAndGet(finalI + finalJ, polynomial1[finalI] * polynomial2[finalJ]);
+                    return null;
+                }));
+            }
+
+        CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size()])).join();
+
+        printPolynomial(product);
+    }
+
     public static void main(String[] args) {
         int[] polynomial1 = {5, 0, 10, 6};
         int[] polynomial2 = {1, 2, 4};
         regularMultiplicationSequential(polynomial1, polynomial2);
+        regularMultiplicationParallel(polynomial1, polynomial2);
     }
 }
