47a0361f15b73041e7ca364f4d4842864180bb89
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-12-06 22:03:00 +0200

    LAB-5: Added sequential n^2 computation

--- Lab-5-Parallelizing-techniques/src/Main.java
+++ Lab-5-Parallelizing-techniques/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+    static void printPolynomial(int[] polynomial) {
+        var stringBuilder = new StringBuilder("%d".formatted(polynomial[0]));
+
+        for (int i = 1; i < polynomial.length; i++) {
+            stringBuilder.append(" + %d%s".formatted(polynomial[i], "x^%d".formatted(i)));
+        }
+
+        System.out.println(stringBuilder);
+    }
+
+    static void regularMultiplicationSequential(int[] polynomial1, int[] polynomial2) {
+        int polynomial1Degree = polynomial1.length;
+        int polynomial2Degree = polynomial2.length;
+
+        int[] product = new int[polynomial1Degree + polynomial2Degree - 1];
+        for (int i = 0; i < product.length; i++)
+            product[i] = 0;
+
+        for (int i = 0; i < polynomial1Degree; i++)
+            for (int j = 0; j < polynomial2Degree; j++)
+                product[i + j] += polynomial1[i] * polynomial2[j];
+
+        printPolynomial(product);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] polynomial1 = {5, 0, 10, 6};
+        int[] polynomial2 = {1, 2, 4};
+        regularMultiplicationSequential(polynomial1, polynomial2);
     }
 }
