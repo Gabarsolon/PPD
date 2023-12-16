5de62cff7872d23c14d87cb0476a782abef8c3e5
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-12-16 17:40:52 +0200

    LAB-6: Added function for generating random graph

--- Lab-6-Parallelizing-techniques-2-parallel-explore/src/Main.java
+++ Lab-6-Parallelizing-techniques-2-parallel-explore/src/Main.java
@@ -1,14 +1,11 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
+    static Integer START_VERTEX = 0;
+    static Integer totalNumberOfEdges = 20;
     static Integer numberOfVerticies = 5;
-    static Map<Integer, List<Integer>> outboundEdgesMap = Map.of(
-            0, List.of(1),
-            1, List.of(2, 3),
-            2, List.of(4),
-            3, List.of(2),
-            4, List.of(0)
-    );
+    static Map<Integer, List<Integer>> outboundEdgesMap;
 
     static boolean hamCycleUtil(List<Integer> cycle, Map<Integer, Boolean> visitedVertices, Integer startVertex, Integer lastAddedVertex) {
         if (cycle.size() == numberOfVerticies) {
@@ -51,18 +48,59 @@ public class Main {
         return cycle;
     }
 
-    public static void main(String[] args) {
-        var hamiltonianCycle = getHamiltonianCycleStartingFromGivenVertex(0);
-
+    public static void printHamiltonianCycle(List<Integer> hamiltonianCycle) {
         if (hamiltonianCycle == null)
             System.out.println("There isn't any hamiltonian cycle");
         else
-            System.out.println("A hamiltonian cycle corresponding to the graph is:\n %s".formatted(
+            System.out.printf(
+                    "A hamiltonian cycle corresponding to the graph is:\n %s%n",
                     hamiltonianCycle
                             .toString()
                             .replace("[", "")
                             .replace("]", "")
                             .replace(",", "->")
-            ));
+            );
+    }
+
+    static void generateRandomGraph() {
+        var random = new Random();
+        var currentNumberOfAddedEdges = 0;
+        outboundEdgesMap = new HashMap<>();
+
+        for (Integer vertex = 0; vertex < numberOfVerticies; vertex++) {
+            List<Integer> listOfVertices = new ArrayList<>();
+
+            for (int currentVertex = random.nextInt(0, numberOfVerticies / 2);
+                 currentVertex < numberOfVerticies && currentNumberOfAddedEdges < totalNumberOfEdges;
+                 currentVertex += random.nextInt(1, numberOfVerticies / 2 + 1)
+            ) {
+                listOfVertices.add(currentVertex);
+                currentNumberOfAddedEdges++;
+            }
+
+            outboundEdgesMap.put(vertex, listOfVertices);
+        }
+    }
+
+    public static void main(String[] args) {
+//        outboundEdgesMap = Map.of(
+//                0, List.of(1),
+//                1, List.of(2, 3),
+//                2, List.of(4),
+//                3, List.of(2),
+//                4, List.of(0)
+//        );
+
+        generateRandomGraph();
+        System.out.println(outboundEdgesMap.entrySet());
+        System.out.println("-----------------------------------------------------");
+
+        var startTime = System.nanoTime();
+        var hamiltonianCycle = getHamiltonianCycleStartingFromGivenVertex(START_VERTEX);
+        var endTime = System.nanoTime();
+
+        System.out.printf("Regular multiplication sequential finished in: %dms\n", (endTime - startTime) / 1000000);
+        System.out.println("--------------------------------------------------------");
+        printHamiltonianCycle(hamiltonianCycle);
     }
 }
