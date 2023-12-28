0b566f23a128a45464335067d2c166a21035d354
917_Turcu_Remus <78847479+Gabarsolon@users.noreply.github.com>
2023-12-28 15:59:58 +0200

    LAB-6: Added some kind parallel implementation

--- Lab-6-Parallelizing-techniques-2-parallel-explore/src/Main.java
+++ Lab-6-Parallelizing-techniques-2-parallel-explore/src/Main.java
@@ -1,13 +1,56 @@
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
+    enum EXECUTION_TYPE {
+        SEQUENTIAL,
+        PARALLEL
+    }
+
     static Integer START_VERTEX = 0;
-    static Integer totalNumberOfEdges = 20;
-    static Integer numberOfVerticies = 5;
+    static Integer totalNumberOfEdges = 400;
+    static Integer numberOfVerticies = 20;
     static Map<Integer, List<Integer>> outboundEdgesMap;
 
-    static boolean hamCycleUtil(List<Integer> cycle, Map<Integer, Boolean> visitedVertices, Integer startVertex, Integer lastAddedVertex) {
+    static boolean hamCycleUtilParallel(List<Integer> cycle, Map<Integer, Boolean> visitedVertices, Integer startVertex, Integer lastAddedVertex) throws ExecutionException, InterruptedException {
+        if (cycle.size() == numberOfVerticies) {
+            if (outboundEdgesMap.get(lastAddedVertex).contains(startVertex)) {
+                cycle.add(startVertex);
+                printHamiltonianCycle(cycle);
+                return true;
+            } else
+                return false;
+        }
+
+        List<CompletableFuture<Boolean>> completableFutures = new ArrayList<>();
+        for (var nextVertex : outboundEdgesMap.get(lastAddedVertex)) {
+            if (visitedVertices.get(nextVertex) == null) {
+                // Create a copy of the cycle and visited vertices
+                List<Integer> updatedCycle = new ArrayList<>(cycle);
+                Map<Integer, Boolean> updatedVisitedVertices = new HashMap<>(visitedVertices);
+
+                updatedCycle.add(nextVertex);
+                updatedVisitedVertices.put(nextVertex, true);
+
+                completableFutures.add(CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return hamCycleUtilParallel(updatedCycle, updatedVisitedVertices, startVertex, nextVertex);
+                    } catch (ExecutionException | InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }));
+            }
+        }
+
+        for(var completableFuture : completableFutures)
+            if(completableFuture.get())
+                return true;
+
+        return false;
+    }
+
+    static boolean hamCycleUtilSequential(List<Integer> cycle, Map<Integer, Boolean> visitedVertices, Integer startVertex, Integer lastAddedVertex) {
         if (cycle.size() == numberOfVerticies) {
             if (outboundEdgesMap.get(lastAddedVertex).contains(startVertex)) {
                 cycle.add(startVertex);
@@ -22,7 +65,7 @@ public class Main {
                 cycle.add(nextVertex);
                 visitedVertices.put(nextVertex, true);
 
-                if (hamCycleUtil(cycle, visitedVertices, startVertex, nextVertex))
+                if (hamCycleUtilSequential(cycle, visitedVertices, startVertex, nextVertex))
                     return true;
 
                 cycle.remove(cycle.size() - 1);
@@ -33,24 +76,30 @@ public class Main {
         return false;
     }
 
-    static List<Integer> getHamiltonianCycleStartingFromGivenVertex(int startVertex) {
+    static List<Integer> getHamiltonianCycleStartingFromGivenVertex(int startVertex, EXECUTION_TYPE executionType) throws ExecutionException, InterruptedException {
         var cycle = new ArrayList<Integer>();
         var visitedVertices = new HashMap<Integer, Boolean>();
 
         cycle.add(startVertex);
         visitedVertices.put(startVertex, true);
 
-        if (!hamCycleUtil(cycle, visitedVertices, startVertex, startVertex)) {
-            System.out.println(cycle);
+        boolean hasHamiltonianCycle;
+        if (executionType == EXECUTION_TYPE.SEQUENTIAL)
+            hasHamiltonianCycle = hamCycleUtilSequential(cycle, visitedVertices, startVertex, startVertex);
+        else
+            hasHamiltonianCycle = hamCycleUtilParallel(cycle, visitedVertices, startVertex, startVertex);
+
+        if (!hasHamiltonianCycle) {
             return null;
         }
 
         return cycle;
     }
 
+
     public static void printHamiltonianCycle(List<Integer> hamiltonianCycle) {
         if (hamiltonianCycle == null)
-            System.out.println("There isn't any hamiltonian cycle");
+            System.out.printf("There isn't any hamiltonian cycle starting from node %d%n", START_VERTEX);
         else
             System.out.printf(
                     "A hamiltonian cycle corresponding to the graph is:\n %s%n",
@@ -82,7 +131,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 //        outboundEdgesMap = Map.of(
 //                0, List.of(1),
 //                1, List.of(2, 3),
@@ -90,16 +139,27 @@ public class Main {
 //                3, List.of(2),
 //                4, List.of(0)
 //        );
+//        numberOfVerticies = 5;
 
         generateRandomGraph();
         System.out.println(outboundEdgesMap.entrySet());
         System.out.println("-----------------------------------------------------");
 
         var startTime = System.nanoTime();
-        var hamiltonianCycle = getHamiltonianCycleStartingFromGivenVertex(START_VERTEX);
+        var hamiltonianCycle = getHamiltonianCycleStartingFromGivenVertex(START_VERTEX, EXECUTION_TYPE.SEQUENTIAL);
         var endTime = System.nanoTime();
 
-        System.out.printf("Regular multiplication sequential finished in: %dms\n", (endTime - startTime) / 1000000);
+        System.out.printf("Sequential execution finished in: %dms\n", (endTime - startTime) / 1000000);
+        printHamiltonianCycle(hamiltonianCycle);
+
+        startTime = System.nanoTime();
+        hamiltonianCycle = getHamiltonianCycleStartingFromGivenVertex(START_VERTEX, EXECUTION_TYPE.PARALLEL);
+        endTime = System.nanoTime();
+
+        System.out.printf("Parallel execution finished in: %dms\n", (endTime - startTime) / 1000000);
+        printHamiltonianCycle(hamiltonianCycle);
+
+
         System.out.println("--------------------------------------------------------");
         printHamiltonianCycle(hamiltonianCycle);
     }
