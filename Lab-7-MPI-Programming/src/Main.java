71f7c26866dc0ccc45684371accd8c6668bc28de
Gabarsolon <remusturcu73@gmail.com>
2024-01-02 18:20:21 +0200

    LAB-7: Added MPI implementation for regular parallel with static polynomial

--- Lab-7-MPI-Programming/src/Main.java
+++ Lab-7-MPI-Programming/src/Main.java
@@ -1,3 +1,5 @@
+import mpi.*;
+
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
@@ -5,9 +7,9 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
 
 public class Main {
     static int MAX_COEFFICIENT = 1000;
-    static int MAX_DEGREE = 100000;
+    static int MAX_DEGREE = 10;
 
-    static boolean PRINT_OUTPUT_FLAG = false;
+    static boolean PRINT_OUTPUT_FLAG = true;
 
     static void printPolynomial(int[] polynomial) {
         var stringBuilder = new StringBuilder("%d".formatted(polynomial[0]));
@@ -29,54 +31,35 @@ public class Main {
         return polynomial;
     }
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-//        int[] polynomial1 = {1, 1, 2};
-//        int[] polynomial2 = {1, 1, 1};
+    public static void main(String[] args) throws ExecutionException, InterruptedException, MPIException {
+        MPI.Init(args);
+        int rank = MPI.COMM_WORLD.getRank();
+        System.out.println("Hi! I am the process of rank " + rank);
+        int[] polynomial1 = {1, 1, 2};
+        int[] polynomial2 = {1, 1, 1};
 
-        int[] polynomial1 = generateRandomPolynomial();
-        int[] polynomial2 = generateRandomPolynomial();
+//        int[] polynomial1 = generateRandomPolynomial();
+//        int[] polynomial2 = generateRandomPolynomial();
 
 //        printPolynomial(polynomial1);
 //        printPolynomial(polynomial2);
-        System.out.println("--------------------------");
+//        System.out.println("--------------------------");
 
         long start, end;
-
-        start = System.nanoTime();
-        int[] product = RegularMultiplication.sequential(polynomial1, polynomial2);
-        end = System.nanoTime();
-
-        System.out.printf("Regular multiplication sequential finished in: %dms\n", (end - start) / 1000000);
-        if (PRINT_OUTPUT_FLAG) printPolynomial(product);
-        System.out.println("---------------------------------------------------------------------------");
-
+        int product[];
 
         start = System.nanoTime();
         product = RegularMultiplication.parallel(polynomial1, polynomial2);
         end = System.nanoTime();
 
-        System.out.printf("Regular multiplication parallel finished in: %dms\n", (end - start) / 1000000);
-        if (PRINT_OUTPUT_FLAG) printPolynomial(product);
-        System.out.println("---------------------------------------------------------------------------");
 
+        if(rank == 0){
+            printPolynomial(RegularMultiplication.sequential(polynomial1, polynomial2));
+            System.out.printf("Regular multiplication parallel finished in: %dms\n", (end - start) / 1000000);
+            if (PRINT_OUTPUT_FLAG) printPolynomial(product);
+            System.out.println("---------------------------------------------------------------------------");
+        }
 
-        start = System.nanoTime();
-        product = KaratsubaMultiplication.multiplySequential(polynomial1, polynomial2);
-        end = System.nanoTime();
-
-        System.out.printf("Karatsuba multiplication sequential finished in: %dms\n", (end - start) / 1000000);
-        if (PRINT_OUTPUT_FLAG) printPolynomial(product);
-        System.out.println("---------------------------------------------------------------------------");
-
-
-        start = System.nanoTime();
-        product = KaratsubaMultiplication.multiplyParallel(polynomial1, polynomial2);
-        end = System.nanoTime();
-
-        System.out.printf("Karatsuba multiplication parallel finished in: %dms\n", (end - start) / 1000000);
-        if (PRINT_OUTPUT_FLAG) printPolynomial(product);
-        System.out.println("---------------------------------------------------------------------------");
-
-
+        MPI.Finalize();
     }
 }
--- Lab-7-MPI-Programming/src/RegularMultiplication.java
+++ Lab-7-MPI-Programming/src/RegularMultiplication.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicIntegerArray;
+import mpi.*;
+
 
 public class RegularMultiplication {
     static int[] sequential(int[] polynomial1, int[] polynomial2) {
@@ -19,43 +17,72 @@ public class RegularMultiplication {
         return product;
     }
 
-    static int[] parallel(int[] polynomial1, int[] polynomial2) {
+    static int[] parallel(int[] polynomial1, int[] polynomial2) throws MPIException {
+        int rank = MPI.COMM_WORLD.getRank();
+        int size = MPI.COMM_WORLD.getSize();
+
         int polynomial1Degree = polynomial1.length - 1;
         int polynomial2Degree = polynomial2.length - 1;
 
-        var product = new int[polynomial1Degree + polynomial2Degree + 1];
-        var productLength = product.length;
+        var productLength = polynomial1Degree + polynomial2Degree + 1;
         var halfOfProductLength = productLength / 2;
 
-        List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
+        if (rank == 0) {
+            var product = new int[polynomial1Degree + polynomial2Degree + 1];
 
-
-        for (int i = 0; i < halfOfProductLength; i++) {
-            int finalI = i;
-            completableFutures.add(CompletableFuture.runAsync(() -> {
-                for (int j = 0; j <= finalI; j++) {
-                    product[finalI] += polynomial1[j] * polynomial2[finalI - j];
+            int numberOfPositionsFromProductArrayProcessed = 0;
+            for (int i = 0; i < halfOfProductLength; i += size){
+                for (int j = 0; j <= i; j++) {
+                    product[i] += polynomial1[j] * polynomial2[i - j];
                 }
 
-                int finalI2 = productLength - finalI - 1;
+                int finalI2 = productLength - i - 1;
                 int lowerLimit = finalI2 - halfOfProductLength;
                 int upperLimit = finalI2 - lowerLimit;
                 for (int j = lowerLimit; j <= upperLimit; j++) {
                     product[finalI2] += polynomial1[j] * polynomial2[finalI2 - j];
                 }
-            }));
-        }
 
-        if (productLength % 2 != 0) {
-            completableFutures.add(CompletableFuture.runAsync(() -> {
+                numberOfPositionsFromProductArrayProcessed += 2;
+            }
+            if (productLength % 2 != 0) {
                 for (int j = 0; j <= halfOfProductLength; j++) {
                     product[halfOfProductLength] += polynomial1[j] * polynomial2[halfOfProductLength - j];
                 }
-            }));
+                numberOfPositionsFromProductArrayProcessed++;
+            }
+
+            for(int currentNumberOfProcessedPositionsFromArray = numberOfPositionsFromProductArrayProcessed; currentNumberOfProcessedPositionsFromArray< productLength;currentNumberOfProcessedPositionsFromArray++){
+                int[] computedProduct = new int[2];
+                Status status = MPI.COMM_WORLD.recv(computedProduct,2, MPI.INT, MPI.ANY_SOURCE, MPI.ANY_TAG);
+                int positionInProductArray = computedProduct[0];
+                int productCoefficient = computedProduct[1];
+                product[positionInProductArray] = productCoefficient;
+            }
+
+            return product;
         }
 
-        CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[completableFutures.size() - 1])).join();
+        for (int i = rank; i < halfOfProductLength; i += size) {
+            int i_product = 0;
 
-        return product;
+            for (int j = 0; j <= i; j++) {
+                i_product += polynomial1[j] * polynomial2[i - j];
+            }
+
+            MPI.COMM_WORLD.bSend(new int[]{i, i_product}, 2, MPI.INT, 0, 0);
+
+            int symmetricIProduct = 0;
+            int symmetricI = productLength - i - 1;
+            int lowerLimit = symmetricI - halfOfProductLength;
+            int upperLimit = symmetricI - lowerLimit;
+            for (int j = lowerLimit; j <= upperLimit; j++) {
+                symmetricIProduct += polynomial1[j] * polynomial2[symmetricI - j];
+            }
+
+            MPI.COMM_WORLD.bSend(new int[]{symmetricI, symmetricIProduct}, 2, MPI.INT, 0, 0);
+        }
+
+        return null;
     }
 }
